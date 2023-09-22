e136225119ad7301a84debe98659c206356d5c58
wtb-skill <144600320+wtb-skill@users.noreply.github.com>
2023-09-22 12:10:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("Hello World!")
 print("Znajdujesz się teraz w trybie edycji tego pliku – wcielisz się teraz w drugiego programistę, który wprowadza zmiany. 
   Następnie na dole strony wypełnij tytuł commita i potwierdź zapisanie guzikiem “Commit changes”.") 
+
+druga zmiana
